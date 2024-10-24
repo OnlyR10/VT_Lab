d184e0edb88633e1812f8416881a1a7257a21e6a
Kirill Naydovich <kirillnaydovich@gmail.com>
2024-10-24 23:05:35 +0300

    feat: unification

---
+++ Naydovich/Naydovich.UI/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Naydovich.UI.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
